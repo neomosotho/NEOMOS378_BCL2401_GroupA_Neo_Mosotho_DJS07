0902f80fb7ae84b1ef3fbde956cb66ad3a7b1ed0
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-06-01 11:53:39 +0200

    "Removed form tags and replaced with a div element because we are not submitting anything"

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -41,13 +41,13 @@ export default function Meme() {
 
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input type="text" placeholder="Top text" className="form--input" />
 
         <input type="text" placeholder="Bottom text" className="form--input" />
 
         <button className="form--button">Get a new meme image 🖼️</button>
-      </form>
+      </div>
     </main>
   );
 }
