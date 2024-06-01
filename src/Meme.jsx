9aa3c518081fa90c6c159eb3ae104d8309f2a102
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-06-01 11:41:58 +0200

    "Initialize state for meme data"

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 export default function Meme() {
+    // Initialize state for meme data.
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "https://i.imgflip.com/1ur9b0.jpg",
+    });
+
+
     return (
         <main>
             <form className="form">
